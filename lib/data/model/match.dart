26639d41cdb5f772fea3aa1af73aaefd44e6d8b9
donghyuk <donghyuk454@gmail.com>
2023-10-09 20:09:27 +0900

    [FEAT] 경기와 관련 정보를 담는 dto 객체 개발

---
+++ lib/data/model/match.dart
@@ -0,0 +1,14 @@
+class MatchDto {
+  final String title;
+  final String time;
+
+  MatchDto(this.title, this.time);
+
+  String getTitle() {
+    return title;
+  }
+
+  String getTime() {
+    return time;
+  }
+}
