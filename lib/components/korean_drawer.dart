27f7a32ad3abf1d76fff113b65b3af5f58976a1b
donghyuk <donghyuk454@gmail.com>
2023-10-09 20:06:48 +0900

    [FEAT] 축구선수 구독을 위한 drawer 개발

---
+++ lib/components/korean_drawer.dart
@@ -0,0 +1,56 @@
+import 'package:dream/components/player_toggle.dart';
+import 'package:flutter/material.dart';
+
+class KoreanDrawer extends StatefulWidget {
+  List<String> koreanList = ["손흥민", "황희찬", "이강인", "김민재",];
+  List<bool> koreanActiveList = [true, true, false, false,];
+
+  KoreanDrawer({super.key});
+
+  @override
+  State<KoreanDrawer> createState() => _KoreanDrawerState();
+}
+
+class _KoreanDrawerState extends State<KoreanDrawer> {
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> componentList = [];
+
+    for (int i = 0; i < widget.koreanList.length; i++) {
+      newOnChanged(v) {
+        onChanged(v, i);
+      }
+      var component = PlayerToggle(playerName: widget.koreanList[i], isChecked: widget.koreanActiveList[i], onChanged: newOnChanged,);
+      componentList.add(component);
+    }
+
+    ListView koreanListView = ListView.builder(
+      itemCount: componentList.length,
+      itemBuilder: (context, index) {
+        return componentList[index];
+      },
+    );
+
+    return DrawerHeader(
+      child: Column (
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(3, 10, 0, 15),
+            child: Text("구독할 한국 선수"),
+          ),
+          Divider(thickness: 2,),
+          Flexible(
+            child: koreanListView
+          )
+        ],
+      ),
+    );
+  }
+
+  onChanged(v, i) {
+    setState(() {
+      widget.koreanActiveList[i] = v;
+    });
+  }
+}
