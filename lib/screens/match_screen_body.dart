d67b2034434f6128e6f3afb62bd1992540c33157
donghyuk <donghyuk454@gmail.com>
2023-10-11 01:10:44 +0900

    [FEAT] 경기 상세 화면 body 생성

---
+++ lib/screens/match_screen_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MatchScreenBody extends StatelessWidget {
+  const MatchScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("gg"),
+    );
+  }
+}
