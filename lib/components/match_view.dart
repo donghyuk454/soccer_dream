5190339ab39d30f4efb0f4d225bac75378e19ef1
donghyuk <donghyuk454@gmail.com>
2023-10-11 01:05:47 +0900

    [FEAT] MatchView 클릭 시 화면 이동 기능 개발

--- lib/components/match_view.dart
+++ lib/components/match_view.dart
@@ -1,3 +1,4 @@
+import 'package:dream/screens/match_screen.dart';
 import 'package:flutter/material.dart';
 
 class MatchView extends StatelessWidget {
@@ -9,23 +10,34 @@ class MatchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      height: 40,
-      margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black26),
-        borderRadius: BorderRadius.horizontal(
-          //left: Radius.circular(10),
-          right: Radius.circular(10),
-        )
+    return GestureDetector(
+      onTap: () {
+        _navigateToNewScreen(context); // 화면 전환 함수 호출
+      },
+      child: Container(
+        width: 150,
+        height: 40,
+        margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black26),
+          borderRadius: BorderRadius.horizontal(
+            right: Radius.circular(10),
+          )
+        ),
+        child: Column(
+          children: [
+            Text(title),
+            Text(time),
+          ],
+        ),
       ),
-      child: Column(
-        children: [
-          Text(title),
-          Text(time),
-        ],
-      ),
+    );
+  }
+
+  void _navigateToNewScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MatchScreen()),
     );
   }
 }
