915234ab44ae50c476346163694b3194d2fa3508
donghyuk <donghyuk454@gmail.com>
2023-10-13 01:41:23 +0900

    [FEAT] Match 정보 화면의 body 개발

--- lib/screens/match_screen_body.dart
+++ lib/screens/match_screen_body.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
 
+import '../components/lineup-painter.dart';
+import '../data/model/match.dart';
+
 class MatchScreenBody extends StatelessWidget {
-  const MatchScreenBody({super.key});
+
+  MatchDto match;
+
+  MatchScreenBody({super.key, required this.match});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("gg"),
+
+    var lineUp = Flex(
+      direction: Axis.vertical,
+      children: [
+        Container(
+            margin: EdgeInsets.fromLTRB(100, 0, 100, 100),
+            alignment: Alignment.topCenter,
+            child: CustomPaint(
+              size: Size(300,300),
+              painter: LineupPainter(),
+              child: Image.asset('assets/images/field.jpg'),
+            )
+        ),
+      ]
+       );
+
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(15),
+          child: Text("프리미어리그"),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("home", style: TextStyle(fontSize: 15,),),
+                Text(match.home, style: TextStyle(fontSize: 35,),),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: Text("vs", style: TextStyle(fontSize: 35),),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("away", style: TextStyle(fontSize: 15,),),
+                Text(match.away, style: TextStyle(fontSize: 35),)
+              ],
+            )
+          ],
+        ),
+        Divider(),
+        Flex(direction: Axis.vertical, children: [lineUp],)
+      ],
     );
   }
 }
