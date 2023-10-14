9def49a5021e2d6110535d80a96fcb54d18040f6
donghyuk <donghyuk454@gmail.com>
2023-10-14 19:46:24 +0900

    [FEAT] 라인업 내의 선수 사이즈에 맞게 등번호가 표시되도록 변경

--- lib/components/lineup-painter.dart
+++ lib/components/lineup-painter.dart
@@ -1,25 +1,63 @@
 import 'package:flutter/material.dart';
 
 class LineupPainter extends CustomPainter {
+
+  double textScaleFactor = 1.0;
+  final int number = 7;
+  final Color circleColor = Colors.red;
+
   @override
   void paint(Canvas canvas, Size size) {
-    final Paint paint = Paint()
-      ..color = Colors.red
+    final paint = Paint()
+      ..color = circleColor
       ..style = PaintingStyle.fill;
 
-    final double centerX = size.width / 2;
-    final double centerY = size.height / 2;
-    final double radius = size.width / 4; // 동그란 원의 반지름
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 15;
 
-    print(centerX);
-    print(centerY);
-    print(radius);
+    canvas.drawCircle(center, radius, paint);
 
-    canvas.drawCircle(Offset(centerX, centerY), radius, paint);
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: number.toString(),
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: radius / 2, // 숫자 크기 조절
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+
+    final textX = center.dx - textPainter.width / 2;
+    final textY = center.dy - textPainter.height / 2;
+
+    textPainter.paint(canvas, Offset(textX, textY));
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
+  }
+
+  // 원의 중앙에 텍스트를 적음.
+  void drawText(Canvas canvas, Size size, String text) {
+    double fontSize = getFontSize(size, text);
+    TextSpan sp = TextSpan(style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold, color: Colors.black), text: text); // TextSpan은 Text위젯과 거의 동일하다.
+    TextPainter tp = TextPainter(text: sp, textDirection: TextDirection.ltr);
+    tp.layout(); // 필수! 텍스트 페인터에 그려질 텍스트의 크기와 방향를 정함.
+    double dx = size.width / 2 - tp.width / 2;
+    double dy = size.height / 2 - tp.height / 2;
+    Offset offset = Offset(dx, dy);
+    tp.paint(canvas, offset);
+  }
+  // 화면 크기에 비례하도록 텍스트 폰트 크기를 정함.
+  double getFontSize(Size size, String text) {
+    return size.width / text.length * textScaleFactor;
   }
 }
