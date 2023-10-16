feaa8fc03f58b83c2b5d068b0d63a57d15826b3b
donghyuk <donghyuk454@gmail.com>
2023-10-17 00:45:52 +0900

    [FEAT] MatchView 에 match color 추가 및 alignment 지정

--- lib/components/match_view.dart
+++ lib/components/match_view.dart
@@ -15,23 +15,39 @@ class MatchView extends StatelessWidget {
       onTap: () {
         _navigateToNewScreen(context); // 화면 전환 함수 호출
       },
-      child: Container(
-        width: 150,
-        height: 40,
-        margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black26),
-          borderRadius: BorderRadius.horizontal(
-            right: Radius.circular(10),
-          )
-        ),
-        child: Column(
-          children: [
-            Text(match.title),
-            Text(match.time),
-          ],
-        ),
-      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            flex: 1,
+              child: Container(
+                width: 10,
+                height: 60,
+                margin: EdgeInsets.only(top: 20),
+                decoration: BoxDecoration(
+                  color: match.color
+                ),
+              )
+          ),
+          Container(
+            width: 200,
+            height: 60,
+            margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black26),
+                borderRadius: BorderRadius.horizontal(
+                  right: Radius.circular(7),
+                )
+            ),
+            child: Column(
+              children: [
+                Text(match.title),
+                Text(match.time),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 
