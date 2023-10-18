98f93f35b3133293f3cede15973c0d987c0a408c
donghyuk <donghyuk454@gmail.com>
2023-10-19 00:15:36 +0900

    [FEAT] dispose 함수를 사용해 저장버튼 클릭시에만 state 가 저장될 수 있게 변경

--- lib/components/korean_drawer.dart
+++ lib/components/korean_drawer.dart
@@ -2,8 +2,10 @@ import 'package:dream/components/player_toggle.dart';
 import 'package:flutter/material.dart';
 
 class KoreanDrawer extends StatefulWidget {
-  final List<String> koreanList = ["손흥민", "황희찬", "이강인", "김민재",];
-  final List<bool> koreanActiveList = [true, true, false, false,];
+  final List<String> koreanList = ["손흥민", "황희찬", "이강인", "김민재", "손흥민", "황희찬", "이강인", "김민재", "손흥민", "황희찬", "이강인", "김민재",];
+  List<bool> koreanActiveList = [true, true, false, false,true, true, false,false,true, true, false, false,];
+  List<bool> koreanActiveBefore = [true, true, false, false,true, true, false,false,true, true, false, false,];
+  bool isSaved = false;
 
   KoreanDrawer({super.key});
 
@@ -32,26 +34,59 @@ class _KoreanDrawerState extends State<KoreanDrawer> {
       },
     );
 
-    return DrawerHeader(
-      child: Column (
-        children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(10, 10, 0, 10),
-            alignment: Alignment.centerLeft,
-            child: Text("구독할 한국 선수"),
-          ),
-          Divider(thickness: 2,),
-          Flexible(
-            child: koreanListView
-          )
-        ],
-      ),
-    );
+    return
+      DrawerHeader(
+        child: Column (
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 10, 0, 10),
+              alignment: Alignment.centerLeft,
+              child: Text("구독할 한국 선수"),
+            ),
+            Divider(thickness: 2,),
+            Flexible(
+                child: koreanListView
+            ),
+            Container(
+              alignment: Alignment.bottomRight,
+              child: TextButton(
+                child: Text('저장'),
+                onPressed: () {
+                  saveState();
+                  Navigator.pop(context);
+                },
+              ),
+            )
+          ],
+        ),
+      );
+  }
+
+  @override
+  void dispose() {
+    if (!widget.isSaved) {
+      loadState();
+    }
+    super.dispose();
   }
 
   onChanged(v, i) {
     setState(() {
       widget.koreanActiveList[i] = v;
     });
+  }
+
+  loadState() {
+    print("loaded");
+    for (var i = 0; i < widget.koreanActiveList.length; i++) {
+      widget.koreanActiveList[i] = widget.koreanActiveBefore[i];
+    }
+  }
+
+  saveState() {
+    print("saved");
+    for (var i = 0; i < widget.koreanActiveList.length; i++) {
+      widget.koreanActiveBefore[i] = widget.koreanActiveList[i];
+    }
   }
 }
