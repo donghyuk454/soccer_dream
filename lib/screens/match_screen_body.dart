1633652808473a1db4b9ffe0496240929e5a6b66
donghyuk <donghyuk454@gmail.com>
2023-10-19 21:47:52 +0900

    [FEAT] 이미지 스크롤 기능 개발

--- lib/screens/match_screen_body.dart
+++ lib/screens/match_screen_body.dart
@@ -4,72 +4,65 @@ import '../components/lineup-painter.dart';
 import '../data/model/match.dart';
 
 class MatchScreenBody extends StatelessWidget {
-
-  MatchDto match;
+  final MatchDto match;
 
   MatchScreenBody({super.key, required this.match});
 
   @override
   Widget build(BuildContext context) {
 
-    var lineUp = SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
-        children: [
-          Container(
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.topCenter,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/field.jpg'),
-                    // fit: BoxFit.fill
-                  )
-              ),
-              child: CustomPaint(
-                size: Size(400,400),
-                painter: LineupPainter(),
-              )
-          ),
-        ],
-      )
-
+    var lineup = Container(
+      margin: EdgeInsets.all(10),
+      alignment: Alignment.topCenter,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/field.jpg'),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: CustomPaint(
+        size: Size(1000, 1000), // 이미지 크기 조절
+        painter: LineupPainter(),
+      ),
     );
 
     return Column(
       children: [
-        Column(
+        Container(
+          margin: EdgeInsets.all(15),
+          child: Text("Premier League"),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              margin: EdgeInsets.all(15),
-              child: Text("Premier League"),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text("home", style: TextStyle(fontSize: 15,),),
-                    Text(match.home, style: TextStyle(fontSize: 35,),),
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                  child: Text("vs", style: TextStyle(fontSize: 35),),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text("away", style: TextStyle(fontSize: 15,),),
-                    Text(match.away, style: TextStyle(fontSize: 35),)
-                  ],
-                )
+                Text("home", style: TextStyle(fontSize: 15),),
+                Text(match.home, style: TextStyle(fontSize: 35),),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: Text("vs", style: TextStyle(fontSize: 35),),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("away", style: TextStyle(fontSize: 15),),
+                Text(match.away, style: TextStyle(fontSize: 35),),
               ],
             ),
           ],
         ),
         Divider(),
-        lineUp
+        Expanded(
+          child: ListView(
+            children: [
+              lineup
+            ],
+          ),
+        ),
       ],
     );
   }
